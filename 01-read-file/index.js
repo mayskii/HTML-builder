13eadb1e5d2c326c810eb02e8f4d4f288e2cd5d0
Maya Zakharova <may@Mayas-MacBook-Pro.local>
2025-01-15 14:25:54 -0600

    feat: fix first task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,9 @@
 const fs = require('fs');
+const path = require('path');
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+readStream.on('data', (data) => {
+    process.stdout.write(data);
+});
+readStream.on('error', (error) => {
+    console.error('Error reading file:', error.message);
+});
