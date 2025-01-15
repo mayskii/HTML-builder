964759745ed375ce5d7d66fb18be652ec9afe61f
Maya Zakharova <may@Mayas-MacBook-Pro.local>
2025-01-15 16:45:50 -0600

    feat: fix second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt')
+const writeStream = fs.createWriteStream(filePath, {flags: 'a'});
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Welcome! Enter your message please...');
+
+rl.on('line', (input) => {
+    if (input.trim().toLowerCase() === 'exit'){
+        console.log('Goodbye! Have a good day!');
+        rl.close();
+        process.exit();
+    } else {
+        writeStream.write(input + '\n')
+    }
+})
