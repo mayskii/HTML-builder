3d2f282b6c66d7ecb32ac71ebc064757ceb479b4
Maya Zakharova <may@Mayas-MacBook-Pro.local>
2025-01-15 18:27:12 -0600

    feat: fix third  task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function displayFileInfo() {
+    const folderPath = path.join(__dirname, 'secret-folder');
+
+    try {
+        const files = await fs.readdir(folderPath, { withFileTypes: true});
+
+        for (const file of files) {
+            const fullPath = path.join(folderPath, file.name);
+
+            if (file.isFile()) {
+
+                const fileInfo = await fs.stat(fullPath);
+
+                const fileExtension = path.extname(file.name).slice(1);
+
+                const fileSize = (fileInfo.size / 1024).toFixed(3);
+
+                console.log(`${file.name} - ${fileExtension} - ${fileSize}kb`);
+            }
+        }
+    } catch (error) {
+        console.error('Error reading the folder', error.message);
+    }
+}
+
+displayFileInfo();
